92d6532ae090ba88c8331ee9dfc7c8b98ca72c84
root <root@mahiti-Desktop>
2014-04-18 11:41:57 +0530

    Cpanel commited

---
+++ laravel/workbench/mahiti/cpanel/src/Mahiti/Cpanel/CpanelServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Mahiti\Cpanel;
+
+use Illuminate\Support\ServiceProvider;
+
+class CpanelServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('mahiti/cpanel');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
